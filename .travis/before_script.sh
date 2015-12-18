0e1797fdd7a83dde919d1941bbf98880dd1d6967
Adam Chainz <adam@adamj.eu>
2015-12-18 13:50:04 +0000

    Travis test on container infrastructure
    MariaDB is now available as an addon, so we should be able to use containers for faster tests.

--- .travis/before_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Install MariaDB to replace the existing MySQL
-sudo service mysql stop
-sudo apt-get install -y python-software-properties
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-sudo add-apt-repository "deb http://ftp.osuosl.org/pub/mariadb/repo/10.0/ubuntu precise main"
-sudo apt-get update -qq
-yes Y | sudo apt-get install -y mariadb-server libmariadbclient-dev
