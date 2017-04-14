80dee3ce10c28b69ca401d30c40603c0600d4733
Adam Chainz <me@adamj.eu>
2017-04-14 10:34:50 +0100

    Remove unused 'setup.py publish'

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-import sys
 
 from setuptools import find_packages, setup
 
@@ -16,19 +15,6 @@ def get_version(package):
 
 
 version = get_version('mariadb_dyncol')
-
-
-if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload dist/*")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
-
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
