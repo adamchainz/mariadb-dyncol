92cfe5c9fe41dfb302fd3dc1d771d9b1b0d0f6b6
Adam Johnson <me@adamj.eu>
2018-10-18 23:49:55 +0100

    Add python_requires to setup.py (#153)

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'six>=1.9.0,<2.0',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license="BSD",
     zip_safe=False,
     keywords='MariaDB',
