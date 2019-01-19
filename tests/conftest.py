9b558d0fff17f73886f227c17e17d3c5f24fbf0e
Adam Johnson <me@adamj.eu>
2019-01-19 12:57:20 +0000

    Upgrade requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from hypothesis import settings
 
 local_settings = settings(
     max_examples=int(os.getenv('HYPOTHESIS_MAX_EXAMPLES', 100)),
-    perform_health_check=False,
 )
 settings.register_profile("fuzztests", local_settings)
 settings.load_profile("fuzztests")
