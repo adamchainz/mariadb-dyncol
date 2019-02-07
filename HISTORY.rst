a47736e98437a57e9a4a20d7097d8febc75ff125
Adam Johnson <me@adamj.eu>
2019-02-07 19:55:18 +0000

    Version 3.0.0

--- HISTORY.rst
+++ HISTORY.rst
@@ -8,6 +8,9 @@ Pending release
 
 .. Insert new release notes below this line
 
+3.0.0 (2019-02-07)
+------------------
+
 * Drop Python 2 support, only Python 3.4+ is supported now.
 
 2.0.0 (2018-10-20)
--- mariadb_dyncol/__init__.py
+++ mariadb_dyncol/__init__.py
@@ -2,6 +2,6 @@ from .base import DynColLimitError, DynColNotSupported, DynColTypeError, DynColV
 
 __author__ = 'Adam Johnson'
 __email__ = 'me@adamj.eu'
-__version__ = '2.0.0'
+__version__ = '3.0.0'
 
 __all__ = ('DynColLimitError', 'DynColNotSupported', 'DynColTypeError', 'DynColValueError', 'pack', 'unpack')
