b79bb4b917b4d6a95b17efd4513737f80387638c
Adam Johnson <me@adamj.eu>
2020-08-31 08:46:26 +0100

    Upgrade requirements (#253)

--- benchmark.py
+++ benchmark.py
@@ -63,9 +63,9 @@ def captured_output(stream_name):
 def captured_stdout():
     """Capture the output of sys.stdout:
 
-       with captured_stdout() as stdout:
-           print("hello")
-       self.assertEqual(stdout.getvalue(), "hello\n")
+    with captured_stdout() as stdout:
+        print("hello")
+    self.assertEqual(stdout.getvalue(), "hello\n")
     """
     return captured_output("stdout")
 
